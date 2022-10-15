024125c826f0fc36aba56a0bda1b813089f695b2
mingweisoo <mingwei.soo@fdmgroup.com>
2022-10-15 19:06:12 +0800

    complete portfolio website

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,27 @@
+import Proj1 from "../assets/ThisSpellsTrouble.png";
+import Proj2 from "../assets/ChatbotDashboard.png";
+import Proj3 from "../assets/StarkTradingForexPlatform.png";
+import Proj4 from "../assets/ClinichiongProject.png";
+
+export const ProjectList = [
+  {
+    name: "This Spells Trouble",
+    image: Proj1,
+    skills: "Unity, C#",
+  },
+  {
+    name: "Chatbot Dashboard",
+    image: Proj2,
+    skills: "React,Node.js, Express.js, Bootstrap, Javascript, HTML, CSS",
+  },
+  {
+    name: "Stark Forex Trading Platform",
+    image: Proj3,
+    skills: "Java Spring, Java, Bootstrap, HTML, CSS",
+  },
+  {
+    name: "Clinichiong UI/UX Project",
+    image: Proj4,
+    skills: "Figma",
+  },
+];
